d64e41f36480068e50edcff92f7c258f95dc5772
inkkei <51513928+inkkei@users.noreply.github.com>
2023-12-09 21:36:59 -0700

    First commit quiz

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import "./index.scss";
+import { questions } from "./questions.js";
+
+function Result({ correct }) {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>
+        Вы отгадали {correct} ответа из {questions.length}
+      </h2>
+      <a href="/">
+        <button>Попробовать снова</button>
+      </a>
+    </div>
+  );
+}
+
+function Game({ step, question, onClickVariant }) {
+  const percentage = Math.round((step / questions.length) * 100);
+  return (
+    <>
+      <div className="progress">
+        <div
+          style={{ width: `${percentage}%` }}
+          className="progress__inner"
+        ></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {question.variants.map((variant, index) => (
+          <li key={index} value={index} onClick={(e) => onClickVariant(e)}>
+            {variant}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function App() {
+  const [step, setStep] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const question = questions[step];
+
+  const onClickVariant = (e) => {
+    setStep(step + 1);
+    if (question.correct === e.target.value) {
+      setCorrect(correct + 1);
+    }
+  };
+
+  return (
+    <div className="App">
+      {step < questions.length ? (
+        <Game step={step} question={question} onClickVariant={onClickVariant} />
+      ) : (
+        <Result correct={correct} />
+      )}
+    </div>
+  );
+}
+
+export default App;
